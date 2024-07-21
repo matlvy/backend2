657b6dacf7cb36abd74e8c7a6e1e31e8b099fcd8
matlvy <145528395+matlvy@users.noreply.github.com>
2024-07-21 01:08:56 -0300

    2.0

--- src/database.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-mongoose
-  .connect(
-    "mongodb+srv://mattlevyprg:coderhouse@cluster0.0nikfi0.mongodb.net/ecommerce"
-  )
-  .then(() => console.log("conexion exitosa"))
-  .catch((error) => console.log("tenemos un error", error));
