2c8166a93c7ef9263b872ae399b22bf0ce05bdd7
matlvy <145528395+matlvy@users.noreply.github.com>
2024-07-22 00:55:42 -0300

    2.1.1

--- src/server.js
+++ src/server.js
@@ -1,35 +1,29 @@
 import express from "express";
+import session from "express-session";
 import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = 5000;
 
 // Express Config
-//app.use(cookieParser());
-app.use(cookieParser("cod3rs3cr3t"));
-
-//CRUD Cookies
-app.get("/set-cookie", (req, res) => {
-  res.cookie("coderCookie", "Mi Nueva Cookie", {
-    maxAge: 100000,
-  });
-  res.json({ message: "Cookie set" });
+app.use(
+  session({
+    secret: "s3cr3t",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+//Session
+app.get("/session", (req, res) => {
+  console.log(req.session);
+  if (req.session.counter) {
+    req.session.counter++;
+    res.send(`${req.session.counter}`);
+  } else {
+    req.session.counter = 1;
+    res.send(`${req.session.counter}`);
+  }
 });
-app.get("/set-signed-cookie", (req, res) => {
-  res.cookie("signedCookie", "my signed cookie", {
-    maxAge: 100000,
-    signed: true,
-  });
-  res.json({ message: "signed cookie set" });
-});
-app.get("/get-cookies", (req, res) => {
-  res.json({ cookies: req.cookies, signed: req.signedCookies });
-});
-app.get("/delete-cookie", (req, res) => {
-  res.clearCookie("coderCookie");
-  res.json({ message: "cookie deleted" });
-});
-
 //Start Server
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
