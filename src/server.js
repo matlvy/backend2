8a1e26b4c6af89a848e35e6a90a04a8c7aac79a1
matlvy <145528395+matlvy@users.noreply.github.com>
2024-07-23 23:01:07 -0300

    2.2

--- src/server.js
+++ src/server.js
@@ -1,19 +1,24 @@
 import express from "express";
 import session from "express-session";
 import cookieParser from "cookie-parser";
+import FileStore from "session-file-store";
+
+const fileStorage = FileStore(session);
 
 const app = express();
 const PORT = 5000;
 
 // Express Config
+app.use(cookieParser());
 app.use(
   session({
+    store: new fileStorage({ path: "./sessions", ttl: 10000 }),
     secret: "s3cr3t",
-    resave: true,
-    saveUninitialized: true,
+    resave: false,
+    saveUninitialized: false,
   })
 );
-//Session
+//Routes
 app.get("/session", (req, res) => {
   console.log(req.session);
   if (req.session.counter) {
@@ -24,38 +29,6 @@ app.get("/session", (req, res) => {
     res.send(`welcome`);
   }
 });
-app.get("/login", (req, res) => {
-  const { username, password } = req.query;
-
-  if (username !== "admin" || password !== "1234") {
-    return res.status(401).send("Invalid username or password");
-  }
-
-  req.session.username = username;
-  req.session.admin = true;
-  res.send("Login success");
-});
-
-app.get("/profile", auth, (req, res) => {
-  res.send(`Welcome ${req.session.username}`);
-});
-
-app.get("/logout", (req, res) => {
-  req.session.destroy();
-  res.send("Sesión cerrada");
-});
-
-// middlewares/auth.middleware.js
-function auth(req, res, next) {
-  if (!req.session.username) {
-    return res.status(401).send("You need to login first");
-  }
-
-  if (req.session.username === "admin" && req.session?.admin) {
-    return next();
-  }
-  res.status(401).send("You need to be an admin");
-}
 
 //Start Server
 app.listen(PORT, () => {
