469c11094371f38de8dfe4ab7582cbe208d9f9b4
matlvy <145528395+matlvy@users.noreply.github.com>
2024-08-03 00:11:59 -0300

    2.5

--- src/server.js
+++ src/server.js
@@ -1,82 +1,37 @@
 import express from "express";
+import indexRoutes from "./routes/index.routes.js";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
 import handlebars from "express-handlebars";
-import { generateToken, authToken } from "./utils.js";
+import viewsRoutes from "./routes/views.routes.js";
 
 const app = express();
+const PORT = 5000;
 
+// Express config
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// Handlebars config
 app.engine(
   "hbs",
   handlebars.engine({ extname: ".hbs", defaultLayout: "main" })
 );
 app.set("view engine", "hbs");
-app.set("views", "./views");
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.set("views", "./src/views");
 
-const users = []; // Persistencia en memoria
+// Mongoose config
+mongoose
+  .connect("mongodb://localhost:27017/practica_integradora")
+  .then(() => console.log("MongoDB connected"))
+  .catch((error) => console.log(error));
 
-app.post("/api/register", (req, res) => {
-  const { name, email, password } = req.body;
-  console.log(name, email, password);
+// Routes config
+app.use("/api", indexRoutes);
+app.use("/", viewsRoutes);
 
-  const exists = users.find((user) => user.email === email);
-
-  if (exists) return res.status(400).json({ error: "User already exists" });
-
-  const user = { name, email, password };
-
-  users.push(user);
-  const accessToken = generateToken(user);
-
-  res.json({ user, accessToken });
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
-
-app.post("/api/login", (req, res) => {
-  const { email, password } = req.body;
-  console.log(users);
-  const user = users.find(
-    (user) => user.email === email && user.password === password
-  );
-
-  if (!user) return res.status(400).json({ error: "Invalid credentials" });
-
-  const accessToken = generateToken(user);
-
-  res.json({ user, accessToken });
-});
-
-app.get("/api/datos", authToken, (req, res) => {
-  res.json({ users });
-});
-
-app.get("/current", authToken, (req, res) => {
-  console.log(req.user);
-  res.json(req.user);
-});
-
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-app.get("/noAutorizado", (req, res) => {
-  res.render("noAutorizado");
-});
-
-app.get("/profile", authToken, (req, res) => {
-  res.render("profile", { user: req.user });
-});
-
-app.get("/register", (req, res) => {
-  res.render("register");
-});
-
-app.get("/login", (req, res) => {
-  res.render("login");
-});
-
-app.get("/page", (req, res) => {
-  res.render("home");
-});
-
-app.listen(3000, () => console.log("Server started on port 3000"));
