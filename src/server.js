7f9d81348b75f719d503b4e1772b015de42d6732
matlvy <145528395+matlvy@users.noreply.github.com>
2024-08-04 00:26:22 -0300

    2.6.1

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import jwt from "jsonwebtoken";
+import passport from "passport";
+import { initializePassport } from "./config/passport.config.js";
 
 const app = express();
 const PORT = 5000;
@@ -8,41 +10,50 @@ const PORT = 5000;
 // Express config
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 app.use(express.static("public"));
+app.use(cookieParser());
+initializePassport();
+app.use(passport.initialize());
 
-//Routes
-app.post("login", (req, res) => {
+// Routes
+app.post("/login", (req, res) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
     return res.status(400).json({
-      error: "falta informacion",
+      error: "Falta información",
     });
   }
-  if (email !== "admin@example.com" || password != "1234") {
+
+  if (email !== "admin@example.com" || password !== "1234") {
     return res.status(401).json({
-      error: "usuario o contrasena incorrecto",
+      error: "Usuario o contraseña incorrecto",
     });
   }
+
   const payload = {
     email,
   };
+
   const token = jwt.sign(payload, "s3cr3t", {
     expiresIn: "2m",
   });
+
   res.cookie("token", token, {
     maxAge: 100000,
+    httpOnly: true,
   });
-  res.json({ message: "login exitoso" });
+
+  res.json({ message: "Login exitoso" });
 });
-app.get("/current", (req, res) => {
-  if (req.cookies.token) {
-    res.json({ message: "Usuario logueado", token: req.cookies.token });
-  } else {
-    res.json({ message: "Usuario no logueado" });
+
+app.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json({ message: "Usuario logueado", user: req.user });
   }
-});
+);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
