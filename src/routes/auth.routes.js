0a95c49b9b660092990251b7b930be77d56ac65a
matlvy <145528395+matlvy@users.noreply.github.com>
2024-08-03 22:18:22 -0300

    2.6

--- src/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import authController from "../controllers/auth.controller.js";
-
-const router = Router();
-
-router.post("/login", authController.login);
-
-export default router;
