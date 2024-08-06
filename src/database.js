a532c8f0f0b912fc9fad2394af772933c021bbb9
matlvy <145528395+matlvy@users.noreply.github.com>
2024-08-05 22:46:41 -0300

    2.7

--- src/database.js
+++ src/database.js
@@ -1,7 +1,5 @@
 import mongoose from "mongoose";
 mongoose
-  .connect(
-    "mongodb+srv://mattlevyprg:coderhouse@cluster0.0nikfi0.mongodb.net/ecommerce"
-  )
+  .connect("mongodb://localhost:27017/backend2/preentrega1")
   .then(() => console.log("conexion exitosa"))
   .catch((error) => console.log("tenemos un error", error));
