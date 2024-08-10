06043a40de97f70b350dc7679f78781431085f85
matlvy <145528395+matlvy@users.noreply.github.com>
2024-08-10 01:33:43 -0300

    2.7.4

--- src/server.js
+++ src/server.js
@@ -1,32 +1,18 @@
 import express from "express";
-import mongoose from "mongoose";
-import morgan from "morgan";
-import passport from "passport";
-import authRoutes from "./routes/auth.routes.js";
-import cookieParser from "cookie-parser";
-import { initializePassport } from "./config/passport.config.js";
+import wordRouter from "./routes/word.router.js";
+import petRouter from "./routes/pet.router.js";
 
 const app = express();
 const PORT = 5000;
 
-// Express config
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(morgan("dev"));
-app.use(cookieParser());
 
-// Passport Config
-initializePassport();
-app.use(passport.initialize());
-
-// Mongoose Config
-mongoose
-  .connect("mongodb://localhost:27017/after_class_2")
-  .then(() => console.log("MongoDB connected"))
-  .catch((error) => console.log(error));
-
-// Routes config
-app.use("/api/auth", authRoutes);
+// Routes
+app.use("/api/dictionary", wordRouter);
+app.use("/api/pets", petRouter);
+app.use("*", (req, res) => {
+  res.status(404).json({ message: "Page not found" });
+});
 
 // Start server
 app.listen(PORT, () => {
