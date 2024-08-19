8fbae04a1224155c1462451eb9d6d46994a1ed69
matlvy <145528395+matlvy@users.noreply.github.com>
2024-08-19 00:23:09 -0300

    2.9

--- src/config/config.js
+++ src/config/config.js
@@ -1,40 +1,9 @@
 import dotenv from "dotenv";
-import { Command } from "commander";
-
-const program = new Command();
-program.option("-m, --mode <string>", "Definir modo del Servidor", "dev");
-
-program.parse();
-
-console.log(program.opts());
-
-const { mode } = program.opts();
-
-let path;
-
-switch (mode) {
-  case "development":
-  case "dev":
-    path = ".env.development";
-    break;
-  case "production":
-    path = ".env.production";
-    break;
-  default:
-    path = ".env";
-    break;
-}
 
 dotenv.config();
 
-console.log(process.env.PORT);
-console.log(process.env.MODE);
-console.log(process.env.MONGO_URI);
-console.log(process.env.JWT_SECRET);
-
 export const config = {
   PORT: process.env.PORT,
-  MODE: process.env.MODE,
   MONGO_URI: process.env.MONGO_URI,
-  JWT_SECRET: process.env.JWT_SECRET,
+  PERSISTANCE: process.env.PERSISTANCE,
 };
--- src/server.js
+++ src/server.js
@@ -1,12 +1,26 @@
 import express from "express";
+import morgan from "morgan";
+import mongoose from "mongoose";
 import { config } from "./config/config.js";
+import routes from "./routes/index.routes.js";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
+// Express config
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
+// Mongoose Config
+mongoose
+  .connect(config.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((error) => console.log(error));
+
+// Routes config
+app.use("/api", routes);
+
+// Start server
 app.listen(config.PORT, () => {
-  console.log(`Server is running on port ${config.PORT}`);
+  console.log(`Server running on port ${config.PORT}`);
 });
