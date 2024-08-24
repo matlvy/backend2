f6fd831183e6a722677aae4325d66985a9d09a4e
matlvy <145528395+matlvy@users.noreply.github.com>
2024-08-24 05:25:59 -0300

    2.12.2

--- src/server.js
+++ src/server.js
@@ -10,7 +10,12 @@ const app = express();
 // Express config
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT"],
+  })
+);
 app.use(morgan("dev"));
 
 // Mongoose config
