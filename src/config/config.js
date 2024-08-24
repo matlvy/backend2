e82d6a0e6c8269391a3c1ec5910a0cb5d9e2142a
matlvy <145528395+matlvy@users.noreply.github.com>
2024-08-23 21:58:53 -0300

    2.12

--- src/config/config.js
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-
-dotenv.config();
-
-export const config = {
-  PORT: process.env.PORT,
-  MONGO_URI: process.env.MONGO_URI,
-  PERSISTANCE: process.env.PERSISTANCE,
-};
--- src/models/toy.model.js
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from "mongoose";
-
-const toySchema = new Schema({
-  name: { type: String, required: true },
-  price: { type: Number, required: true },
-  stock: { type: Number, required: true },
-});
-
-export const toyModel = model("toy", toySchema);
